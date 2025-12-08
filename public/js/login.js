8d91b89215d7177ef206fc01645ab38bbe485fb6
Chris0925-coder <christianguardia@outlook.es>
2025-12-08 03:00:13 -0500

    4

--- public/js/login.js
+++ public/js/login.js
@@ -169,9 +169,9 @@ async function fetchContent() {
   if(!result.error) {    
     // console.log(result.rows[0]);
   // result.rows[0].forEach(res => { 
-    let sumVisitas = result.rows[0][1] + result.rows[0][3];
-    console.log(sumVisitas);
-    content(result.rows[0][0],sumVisitas,result.rows[0][2]);
+    let sumVisitas = result.rows[0][0] + result.rows[0][3];
+    // console.log(sumVisitas);
+    content(sumVisitas,result.rows[0][1],result.rows[0][2]);
     contentB(result.rows[0][3],result.rows[0][5],result.rows[0][4]);
         
       // return await ;
