56a59e07d6ded5de84d0cd427579510cae75c395
Chris0925-coder <christianguardia@outlook.es>
2026-02-24 18:35:57 -0500

    003

--- public/js/pages.js
+++ public/js/pages.js
@@ -134,12 +134,12 @@ async function init(){
     } else {
         document.querySelector(".botonRGPD").addEventListener("click", () => {
             eliminarBloqueRGPD();
-            navigator.sendBeacon(url, JSON.stringify({
-                id: 6,
-                count: 1,
-                domain: dom[2],
-                date: dd,
-            }));
+            // navigator.sendBeacon(url, JSON.stringify({
+            //     id: 6,
+            //     count: 1,
+            //     domain: dom[2],
+            //     date: formatoConZona,
+            // }));
             setCookie("rgpdOK",1,365);
         });
     }
