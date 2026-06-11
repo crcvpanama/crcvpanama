06809902f9dbdf835e34162d9e806f9908c1bed1
Chris0925-coder <christianguardia@outlook.es>
2026-06-11 15:21:44 -0500

    0

--- public/js/login.js
+++ public/js/login.js
@@ -20,7 +20,8 @@ function setCookie(cname, cvalue, exdays) {
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     let expires = "expires="+ d.toUTCString();
 
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    // document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    document.cookie =  `${encodeURIComponent(cname)}=${encodeURIComponent(cvalue)};${expires};path=/`;
 };
 
 function getCookie(cname) {
